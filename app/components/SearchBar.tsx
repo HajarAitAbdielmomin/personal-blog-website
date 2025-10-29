61ef0f33fec2d43530713b983f5aedb2a8f86e1a
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-10-29 20:04:15 +0100

    implement the Search bar component

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,18 @@
+
+export default function SearchBar() {
+    return (
+        <div className="flex justify-center w-full px-4 py-5 bg-transparent">
+            <div className="relative w-full max-w-3xl"> {/* make it long */}
+                <input
+                    type="text"
+                    placeholder="Search blog . . ."
+                    className="block w-full pl-12 pr-4 py-3 text-gray-800 placeholder-gray-400
+                 bg-white/70 border border-gray-300 rounded-full shadow-sm
+                 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400
+                 transition-all duration-300 ease-in-out
+                 hover:shadow-md hover:bg-white/90"
+                />
+            </div>
+        </div>
+    )
+}
