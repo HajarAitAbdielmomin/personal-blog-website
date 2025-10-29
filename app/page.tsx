57eff27fe3a6f1567a76b915e40858aadb20dd51
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-10-29 20:01:00 +0100

    Add main containers and the Navheader components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
-
+import NavigationHeader from "@/app/components/NavigationHeader";
 export default function Home() {
   return (
-    <div className="">
+      <div className="w-full bg-[#F9F5F0] bg-opacity-40">
+          <NavigationHeader/>
+          <div className="h-full min-h-screen w-full container mx-auto py-5 gap-5">
+              {/* Content */}
+          </div>
+      </div>
 
-    </div>
+
   );
 }
