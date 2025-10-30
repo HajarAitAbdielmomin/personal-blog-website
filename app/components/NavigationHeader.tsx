91b145fc809f25e2451bd187df4607152c7cc864
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-10-30 15:39:50 +0100

    Replace max-w-3xl with responsive breakpoints

--- app/components/NavigationHeader.tsx
+++ app/components/NavigationHeader.tsx
@@ -2,11 +2,27 @@ import SearchBar from "./SearchBar";
 export default function NavigationHeader() {
     return (
 
-        <div
-            className="z-50 flex items-center justify-between  bg-opacity-70 w-full min-h-[5rem] shadow px-3 lg:px-28">
+    <div className="relative z-50 flex items-center justify-between w-full min-h-[5rem] bg-white/70 shadow px-4 lg:px-28 backdrop-blur-md">
+
+        <div className="flex items-center gap-2">
+            <img
+                src="/personal-brand-logo.png"
+                alt="Logo"
+                className="h-20 w-auto object-contain"
+            />
+        </div>
+
+        <div className="absolute left-1/2 transform -translate-x-1/2 w-full max-w-2xl">
             <SearchBar/>
         </div>
 
-    )
+        <div>
+            <button className="px-8 py-2 text-sm font-medium text-white bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 rounded-full hover:from-indigo-600 hover:via-purple-600 hover:to-pink-600 transition cursor-pointer shadow-lg">
+                Contact
+            </button>
+        </div>
+
+    </div>
+)
 
 }
