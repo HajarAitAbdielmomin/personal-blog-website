c59c7332bae62d480a677e7a0ea6bb65beb11f2d
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-10-30 18:53:00 +0100

    feat(ui): add large title and descriptive subtitle to main page using font-display typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import NavigationHeader from "@/app/components/NavigationHeader";
 export default function Home() {
   return (
-      <div className="w-full bg-[#F9F5F0] bg-opacity-40">
+      <div className="w-full bg-white/70 bg-opacity-40 font-display">
           <NavigationHeader/>
-          <div className="h-full min-h-screen w-full container mx-auto py-5 gap-5">
-              {/* Content */}
+
+          <div
+              className="h-full min-h-screen w-full container mx-auto py-10 px-6 flex flex-col items-center text-center gap-6">
+
+              <h1 className="text-4xl md:text-5xl font-bold">
+                  Our recent blogs
+              </h1>
+
+              <p className="max-w-5xl text-gray-400 text-base md:text-lg leading-relaxed">
+                  Explore insights, reflections, and stories blending technology, creativity, and life.
+                  Each post is written with care to inspire curiosity and spark thoughtful discussions.
+              </p>
           </div>
       </div>
 
