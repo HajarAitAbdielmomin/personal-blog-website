5360f7a722483d9f9f0a76bb31275dd20ef662cb
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-11-03 13:47:24 +0100

    refactor : remove the date from the card

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -13,23 +13,23 @@ export default function BlogCard(
     }
 ) {
     return (
-        <div className="bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 overflow-hidden font-sans h-[26rem] max-w-sm flex flex-col">
+        <div
+            className="bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 overflow-hidden font-sans h-[26rem] max-w-sm flex flex-col">
             <div className="relative">
                 <img
                     src={image}
                     alt={title}
                     className="w-full h-40 "
                 />
-                <div className="absolute bottom-2 right-2 bg-blue-600 text-white text-sm font-semibold px-3 py-1 rounded-lg">
-                    {date}
-                </div>
+
             </div>
+
 
             <div className="p-5 text-left flex-grow">
                 <h3 className="text-lg font-semibold text-gray-900 mb-2">{title}</h3>
                 <p className="text-gray-400 text-sm mb-4">{desc}</p>
             </div>
-            
+
             <div className="px-5 pb-5 mt-auto text-left">
                 <a href="#" className="text-blue-600 font-medium hover:underline">
                     Read more →
