a8160ba6fcd7d67632a8dda9b13147132c9d5429
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-11-13 11:28:24 +0100

    refactor:Create a BoxMessage to handle API response statuses.

--- app/util/util.tsx
+++ app/util/util.tsx
@@ -1,7 +1,20 @@
+import Swal from "sweetalert2";
+
 export const formatDate = (dateString: string) => {
     const date = new Date(dateString);
     const year = date.getFullYear().toString().slice(-2);
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
     return `${year}-${month}-${day}`;
+};
+
+export const boxMessage = (message: string, type: any) => {
+    return (
+         Swal.fire({
+            icon: type,
+            title: type,
+            text: message,
+            confirmButtonText: 'OK'
+        })
+    );
 };
